1e9e7d88624ff38470d0679318683fbd45a8e1c4
artplnn <artplnn@yandex.ru>
2024-07-21 12:14:38 +0300

    Добавлен README.md, добавлена страница ошибки

--- app/router.py
+++ app/router.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+
+from app.exceptions import CityNotFound
 from app.openmeteo import OpenMeteo
 from app.schemas import SCity
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 templates.env.globals["zip"] = zip
+
 
 @router.get("/", response_class=HTMLResponse)
 def get_index(request: Request):
@@ -15,5 +18,8 @@ def get_index(request: Request):
 
 @router.get("/meteo", response_class=HTMLResponse)
 async def get_meteo(request: Request, city: SCity = Depends()):
-    meteo = await OpenMeteo(city.name).get_meteo()
-    return templates.TemplateResponse(request=request, name="search.html", context={"city": city, "meteo": meteo})
+    try:
+        meteo = await OpenMeteo(city.name).get_meteo()
+        return templates.TemplateResponse(request=request, name="search.html", context={"city": city, "meteo": meteo})
+    except CityNotFound:
+        return templates.TemplateResponse(request=request, name="error.html")
